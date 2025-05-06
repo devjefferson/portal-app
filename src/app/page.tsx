465427ada78136084ec8257f24dd5cc7eb937afc
Jefferson Fonseca <drpcscan@gmail.com>
2025-05-06 15:28:46 -0300

    fix: ajuste front

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,130 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+ <div className="flex gap-8 flex-col min-h-screen">
+  <nav className=" w-full shadow-sm flex items-center justify-center px-4 py-2">
+    <div className="flex justify-between items-center py-4 container">
+    <div className="flex items-center gap-2">
+      
+         <h1 className="text-2xl font-bold tracking-[-.01em]">
+           Logo
+         </h1>
+       </div>
+       <div>
+          <div className=" flex lg:hidden gap-4">
+            <button className=" cursor-pointer border border-solid border-black/[.08] hover:border-1  transition-colors flex items-center justify-center hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]">
+            button 1
+            </button>
+          </div>
+       </div>
+     <div className="hidden lg:flex gap-4">
+    <button className=" cursor-pointer border border-solid border-black/[.08] hover:border-1  transition-colors flex items-center justify-center hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]">
+     button 1
+    </button>
+    <button className=" cursor-pointer border border-solid border-black/[.08] hover:border-1  transition-colors flex items-center justify-center hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]">
+     button 2
+    </button>
+    <button className=" cursor-pointer border border-solid border-black/[.08] hover:border-1  transition-colors flex items-center justify-center hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]">
+     button 3
+    </button>
+    <button className=" cursor-pointer border border-solid border-black/[.08] hover:border-1  transition-colors flex items-center justify-center hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]">
+     button 4
+    </button>
+    <button className=" cursor-pointer border border-solid border-black/[.08] hover:border-1  transition-colors flex items-center justify-center hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]">
+     button 5
+    </button>
+     </div>
+    </div>
+  </nav>
+    <div className="gap-16  font-[family-name:var(--font-geist-sans)] flex justify-center items-center flex-col">
+      <main className="flex flex-col gap-22 row-start-2 p-4  sm:items-start container">
+        <div className="flex flex-col gap-[16px] items-center bg-amber-400 w-full h-[500px] rounded-2xl ">
+          
         </div>
+
+        <div className="grid grid-cols-2 lg:grid-cols-4 gap-[32px] w-full">
+          <div className="w-full h-[170px] lg:h-[300px] bg-blue-900 rounded-2xl"/>
+          <div className="w-full h-[170px] lg:h-[300px] bg-amber-500 rounded-2xl"/>
+          <div className="w-full h-[170px] lg:h-[300px] bg-amber-400 rounded-2xl"/>
+          <div className="w-full h-[170px] lg:h-[300px] bg-purple-800 rounded-2xl"/>
+        </div>
+
+        <div>
+          <div className="h-[48px] w-full lg:w-[450px] bg-gray-300">
+
+          </div>
+        </div>
+        <div className="grid grid-cols-1 lg:grid-cols-4 gap-[32px] w-full">
+          <div className="flex flex-col gap-[28px]">
+            <div className="w-full h-[60px]  bg-blue-900 rounded-xl"/>
+            <div className="h-[200px] w-full  bg-gray-300" />
+            <div className="h-[50px] w-full  bg-gray-300" />
+          </div>
+          <div className="flex flex-col gap-[28px]">
+            <div className="w-full h-[60px]  bg-amber-500 rounded-xl"/>
+            <div className="h-[200px] w-full  bg-gray-300" />
+            <div className="h-[50px] w-full  bg-gray-300" />
+          </div>
+          <div className="flex flex-col gap-[28px]">
+            <div className="w-full h-[60px]  bg-amber-400 rounded-xl"/>
+            <div className="h-[200px] w-full  bg-gray-300" />
+            <div className="h-[50px] w-full  bg-gray-300" />
+          </div>
+          <div className="flex flex-col gap-[28px]">
+            <div className="w-full h-[60px]  bg-purple-800 rounded-xl"/>
+            <div className="h-[200px] w-full  bg-gray-300" />
+            <div className="h-[50px] w-full  bg-gray-300" />
+          </div>
+         
+        </div>
+     
+       <div className="grid grid-cols-1 lg:grid-cols-2 gap-16  w-full">
+          <div className="flex flex-col gap-[28px]">
+          <div className="h-[48px] w-full bg-gray-300"/>
+          <div className="h-[80px] w-full bg-gray-300"/>
+          </div>
+          <div className="h-full w-full flex justify-end items-end">
+          <div className="h-[100px] w-full bg-amber-500 flex justify-center items-center px-10">
+              <div className="h-[20px] w-full bg-white rounded-2xl"></div>
+          </div>
+          </div>
+        </div>
+       
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center bg-blue-800 w-full py-8">
+       <div className="container">
+      <div className="grid grid-cols-1 gap-4 px-4 lg:p-0">
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-[32px] w-full">
+        <div className="flex flex-col gap-6">
+                <div className="h-[24px] w-full lg:w-1/3 bg-white"></div>
+                <div className="flex gap-4">
+                  <div className="h-[54px] w-[54px] bg-white rounded-4xl"/>
+                  <div className="h-[54px] w-[54px] bg-white rounded-4xl"/>
+                  <div className="h-[54px] w-[54px] bg-white rounded-4xl"/>
+                  <div className="h-[54px] w-[54px] bg-white rounded-4xl"/>
+                </div>
+               
+              </div>
+              <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 ">
+                <div className="flex flex-col gap-4"> 
+                <div className="h-[24px] w-full bg-white"/>
+                  <div className="h-[24px] w-2/3 bg-white"/>
+                  <div className="h-[24px] w-2/3 bg-white"/>
+                  <div className="h-[24px] w-2/3 bg-white"/>
+                </div>
+                <div className="flex flex-col gap-4"> 
+                <div className="h-[24px] w-full bg-white"/>
+                  <div className="h-[24px] w-2/3 bg-white"/>
+                  <div className="h-[24px] w-2/3 bg-white"/>
+                  <div className="h-[24px] w-2/3 bg-white"/>
+                </div>
+              </div>
+        </div>
+        <p className="text-white">2025 Portal Fonte. Todos os direitos reservados</p>
+      </div>
+       </div>
+       
       </footer>
     </div>
+ </div>
   );
 }
